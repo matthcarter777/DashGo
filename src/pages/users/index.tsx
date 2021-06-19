612c26a477cd3421506a331e0ee472022ac0d189
Mateus <mateushbsb@gmail.com>
2021-06-18 21:02:17 -0300

    pagination

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,6 +2,7 @@ import { Box, Text, Flex, Heading, Button, Icon, Table, Thead, Tr, Th, Checkbox,
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
 import { Sidebar } from '../../components/Sidebar';
 
 export default function UserList() {
@@ -73,6 +74,8 @@ export default function UserList() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
