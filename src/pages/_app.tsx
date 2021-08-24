93b5e0173e72547d5ba5d216871c7d11b595bd29
Mateus <mateushbsb@gmail.com>
2021-08-24 11:12:09 -0300

    app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from  '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 
 import { theme } from '../styles/theme';
@@ -11,8 +11,6 @@ import { queryClient } from '../services/queryClient';
 if ( process.env.NODE_ENV === 'development') {
   makeServer();
 }
-
-
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
